1832b1d1626d85a19fa7c75e7c5738e013c50903
Justin Yu <xjustinyu@gmail.com>
2018-08-21 16:22:30 +0800

    player moving control complete

---
+++ JackTheGiant/Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 8f;
+    public float maxVelocity = 4f;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMoveKeyboard();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(myBody.velocity.x);
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h > 0)
+        {
+            if (vel < maxVelocity)
+                forceX = speed;
+
+            Vector3 temp = transform.localScale;
+            temp.x = 1.3f;
+            transform.localScale = temp;
+
+            anim.SetBool("Walk", true);
+        }
+        else if (h < 0)
+        {
+            if (vel < maxVelocity)
+                forceX = -speed;
+
+            Vector3 temp = transform.localScale;
+            temp.x = -1.3f;
+            transform.localScale = temp;
+
+            anim.SetBool("Walk", true);
+        }
+        else if (h == 0)
+        {
+            anim.SetBool("Walk", false);
+        }
+
+        myBody.AddForce(new Vector2(forceX, 0));
+    }
+}
