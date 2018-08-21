40f21c62ba1583a17933b796e30bb27811d1dcc7
Justin Yu <xjustinyu@gmail.com>
2018-08-21 17:05:21 +0800

    setting the background

---
+++ JackTheGiant/Assets/Scripts/Background Scripts/BGScaler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Vector3 tempScale = sr.transform.localScale;
+
+        float width = sr.sprite.bounds.size.x;
+
+        float worldHeight = Camera.main.orthographicSize * 2f;
+        float worldWidth = worldHeight / Screen.height * Screen.width;
+
+        tempScale.x = worldWidth / width;
+        sr.transform.localScale = tempScale;
+    }
+}
